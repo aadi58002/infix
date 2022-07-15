c21319dc81fb41f6181543a14d436c2c61451c77
aadi58002 <aadi58002gmail.com>
2022-07-15 17:53:35 +0530

    adding header and toggle theme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,25 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './main.css'
 
-createApp(App).mount('#app')
-
-if (localStorage.theme === 'dark' ||
-    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark');
-}
-
-function theme_toggle(e) {
-    let dark = e.target.checked;
-
-    if (dark) {
-        localStorage.theme = 'dark'
-        document.documentElement.classList.add('dark');
-    } else {
-        localStorage.theme = 'light'
-        document.documentElement.classList.remove('dark');
-    }
-}
-
-document.getElementById("theme-switcher")
-    .addEventListener("change", switchTheme);
-
+createApp(App).mount('#app');
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const config = {
+    content: ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}",],
+    theme: {
+        extend: {},
+    },
+
+    plugins: [
+        function({ addVariant }) {
+            addVariant('child', '& > *');
+            addVariant('child-hover', '& > *:hover');
+        }
+    ],
+    mode: 'jit',
+    darkMode: 'class',
+};
+
+module.exports = config;
