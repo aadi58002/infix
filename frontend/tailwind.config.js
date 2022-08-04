8b139c6410dad2501caf795e0b8c71478778e9d6
aadi58002 <aadi58002gmail.com>
2022-08-04 20:40:49 +0530

    moving to a community template for nuxt

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss/types').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  darkMode: 'class',
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./*.vue",
-    "./plugins/**/*.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './assets/**/*.scss',
+    './assets/**/*.css',
   ],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [
-      require("daisyui"),
-      function({ addVariant }) {
-          addVariant('child', '& > *');
-          addVariant('child-hover', '& > *:hover');
-      }
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
   ],
-  mode: 'jit',
-  darkMode: 'class',
 }
