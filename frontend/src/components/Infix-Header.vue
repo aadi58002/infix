41503d71d8ae68efa963e7eab9df18814a980c8a
aadi58002 <aadi58002gmail.com>
2022-08-05 20:53:05 +0530

    Changing the appearance of the logo part of header

--- frontend/src/components/Infix-Header.vue
+++ frontend/src/components/Infix-Header.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="m-6 border-2 rounded p-2 border-slate-900 flex justify-between dark:border dark:border-white">
     <p class="p-2">
-      ⋩  Hello
+      <span class="text-xl pr-6 font-extrabold">⋩</span>  <span class="text-lg font-extrabold">Infix</span>
     </p>
     <TemColorChange />
   </div>
